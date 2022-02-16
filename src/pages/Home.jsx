13162fe079f748bf527756576c09720942aeb28f
Claudia Londono <claudia@londonos.com>
2022-02-16 10:43:09 -0600

    chaged the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,18 +5,19 @@ function Home(){
             <div className="card">
             <h1>The most recent best goals from around world</h1>
                 <div className="match">
-                    <div className="match_info">
-                        <Link to ={"/competitions"}>
-                            <img src= {process.env.PUBLIC_URL +"/images/competitions.png"}/>
-                            <h1>Competitions</h1>
-                        </Link>
-                    </div>
                     <div className="thumbnail">    
                         <Link to ={"/matches"}>    
                             <img src= {process.env.PUBLIC_URL +"/images/matches.png"}/> 
                             <h1>Matches</h1>
                         </Link>      
                     </div>
+                    <div className="match_info">
+                        <Link to ={"/competitions"}>
+                            <img src= {process.env.PUBLIC_URL +"/images/competitions.png"}/>
+                            <h1>Competitions</h1>
+                        </Link>
+                    </div>
+
                 </div>    
             </div>
         </div> 
